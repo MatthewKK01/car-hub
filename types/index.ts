b28e61cf0f01caab7573622e41ed1cea7e41f27b
Mate Kavelashvili <matthewkk@Mates-MacBook-Air.local>
2023-07-10 19:42:11 +0400

    added custoFilter

--- types/index.ts
+++ types/index.ts
@@ -16,6 +16,14 @@ export interface FilterProps {
     limit: number,
     model: string
 }
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+export interface CustomFilterProps {
+    title: string,
+    options: OptionProps[]
+}
 
 export interface HomeProps {
     searchParams: FilterProps
